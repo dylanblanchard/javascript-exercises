39f954d98f955c14f298472592e004f6eaaf3898
Dylan Blanchard <hi@dylan.pizza>
2020-01-12 23:14:34 -0500

    136 - are we there yet while loop

--- script.js
+++ script.js
@@ -1,35 +1,7 @@
-// Print all numbers between -10 and 19
+var ask = prompt("Are we there yet");
 
-var num1 = -10;
+while (ask !== "yes") {
+    var ask = prompt("Are we there yet?");
 
-while (num1 < 19) {
-    console.log(num1);
-    num1++;
 }
-
-// Print all even numbers between 10 & 40
-var num2 = 12;
-
-while (num2 < 38) {
-    console.log(num2);
-    (num2 = (num2 + 2))
-}
-// Print all odd numbers between 300 & 333
-var num3 = 301;
-
-while (num3 < 331) {
-    console.log(num3);
-    (num3 = (num3 + 2))
-}
-
-// Print all numbers divisible by 5 and 3 between 5 & 50
-
-var num4 = 5;
-
-while (num4 <= 50) {
-    if (num4 % 3 === 0 && num4 % 5 === 0) {
-        console.log(num4);
-
-    }
-    num4 += 1;
-}
+alert("Yay, happy to be there!");
