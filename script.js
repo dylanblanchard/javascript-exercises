e0c8aaf8bbcb7ab231ba41f8af8d835a1ffcf3ce
Dylan Blanchard <hi@dylan.pizza>
2020-01-12 22:59:44 -0500

    while loops 135 exercise

--- script.js
+++ script.js
@@ -1,23 +1,35 @@
-var num = 6;
-var gue = Number(prompt("Guess a number between 1 and 10?"));
+// Print all numbers between -10 and 19
 
-if (gue == num) {
-    console.log("Dang! That's right");
+var num1 = -10;
+
+while (num1 < 19) {
+    console.log(num1);
+    num1++;
 }
 
-if (gue < 1 || gue > 10) {
-    console.log("come on now, colour between the lines")
-    var gue = Number(prompt("Guess a number between 1 and 10?"));
+// Print all even numbers between 10 & 40
+var num2 = 12;
+
+while (num2 < 38) {
+    console.log(num2);
+    (num2 = (num2 + 2))
+}
+// Print all odd numbers between 300 & 333
+var num3 = 301;
+
+while (num3 < 331) {
+    console.log(num3);
+    (num3 = (num3 + 2))
 }
 
-if (gue < num) {
-    console.log("Nope, guess higher");
-    var gue = Number(prompt("Guess a number between 1 and 10?"));
-}
+// Print all numbers divisible by 5 and 3 between 5 & 50
 
-if (gue > num) {
-    console.log("Nope, guess lower");
-    var gue = Number(prompt("Guess a number between 1 and 10?"));
-}
+var num4 = 5;
 
-// This is the guess a number game
+while (num4 <= 50) {
+    if (num4 % 3 === 0 && num4 % 5 === 0) {
+        console.log(num4);
+
+    }
+    num4 += 1;
+}
