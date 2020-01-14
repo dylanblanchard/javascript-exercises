6bb6fbffefb3700c1087b45e219a667fce210f4e
Dylan Blanchard <hi@dylan.pizza>
2020-01-13 23:13:32 -0500

    functions 148 / 149

--- script.js
+++ script.js
@@ -1,27 +1,47 @@
-// for loops
+// javascript functions problem set (148)
 
-//   for (init; condition; step) {
-//run some code
-//  }
-
-// Print all numbers between -10 and 19
-for (var i = -9; i < 19; i++) {
-    console.log(i);
-}
-
-// Print all even numbers between 10 & 40
-for (var i = 12; i < 40; i = i + 2) {
-    console.log(i);
-}
-
-// Print all odd numbers between 300 & 333
-for (var i = 301; i < 333; i = i + 2) {
-    console.log(i);
-}
-
-// Print all numbers divisible by 5 and 3 between 5 & 50
-for (var i = 5; i < 50; i++) {
-    if (i % 5 === 0 && i % 3 === 0) {
-        console.log(i);
+// write isEven() function, looks at numeric argument and returns true if even
+function isEven(x) {
+    if (x % 2 === 0) {
+        return (true);
     }
+    return (false);
 }
+
+// fancier way
+function isEven(num) {
+    return num % 2 === 0
+}
+
+isEven(4);
+isEven(21);
+isEven(68);
+isEven(333);
+
+// write factorial() which takes a numeric argument and returns factorial of that number
+
+function factorial(num) {
+    // define result variable
+    var result = 1;
+    // calculate factorial and store value in result
+    for (var i = 2; i <= num; i++) {
+        result *= i;
+        // this is short version of result = result * i
+    }
+    return result;
+}
+
+factorial(5);
+factorial(2);
+factorial(10);
+factorial(0);
+
+// write function that takes kebab-cased-string and turns it into snake_cased
+function kebabToSnake(str) {
+    var snake = str.replace(/-/g, "_");
+    return (snake);
+}
+
+kebabToSnake("hello-world");
+kebabToSnake("dogs-are-awesome");
+kebabToSnake("blah");
